eb1497dbf081060427f814f88fc7aa2021ac8bdc
Jury <jurypassa@gmail.com>
2021-11-29 18:37:20 +0100

    fine esercizio

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,42 @@
 const app = new Vue ({
     el: "#root",
     data: {
-
+        todos: [
+            {
+                text: "Fare la spesa",
+                done: false
+            },
+            {
+                text: "Fare il bucato",
+                done: true
+            },
+            {
+                text: "portare a spasso il cane",
+                done: false
+            },
+            {
+                text: "Preparare il pranzo",
+                done: true
+            }
+        ],
+        testoInserito: ""
     },
     methods: {
+       rimozione: function(index){
+           this.todos.splice(index, 1)
+       },
+       
+       aggiunta: function(){
+           let nuovoOggetto = {text: this.testoInserito};
+           this.todos.push(nuovoOggetto)
+       },
 
+       inversione: function(index){
+           if(this.todos[index].done == false){
+            this.todos[index].done = true;
+           }else{
+            this.todos[index].done = false;
+           }
+       }
     },
 });
